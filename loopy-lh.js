63bae01fa4fc8d15e0faf69ae153008863a09433
Danny-Tran <truong_tran@live.com>
2019-04-12 17:25:49 +0000

    Add loopy excercise

---
+++ loopy-lh.js
@@ -0,0 +1,16 @@
+for (var num1 = 100; num1 <= 200; num1++) {
+ if (num1 % 3 === 0 && num1 % 4 === 0) {
+ 	console.log("LoopyLighthouse");
+ } else
+ if (num1 % 3 === 0) {
+ 	console.log("Loopy")
+ } else
+ if (num1 % 4 === 0) {
+ 	console.log("Lighthouse")
+ } else {
+ 	console.log(num1)
+ }
+}
+
+
+
